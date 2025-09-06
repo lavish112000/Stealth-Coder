498b8e69b56f7756c404a8c66785f083c760f5c4
Lalit Choudhary <90082082+lavish112000@users.noreply.github.com>
2025-09-06 11:47:59 +0000

    Update layout for Electron app compatibility

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -27,7 +27,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" className={`dark ${inter.variable} ${sourceCodePro.variable}`}>
-      <body>
+      <body className="bg-transparent">
         {children}
         <Toaster />
       </body>
